8f5752b3398a82856daba8bccfc2d67143274301
coral2742 <coral2742@gmail.com>
2024-02-20 14:26:41 +0100

    :sparkles: Commit inicial

---
+++ src/main/java/com/uniovi/sdi2324entrega181/Sdi2324Entrega181Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324entrega181;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324Entrega181Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324Entrega181Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2324entrega181/Sdi2324Entrega181ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324entrega181;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324Entrega181ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
