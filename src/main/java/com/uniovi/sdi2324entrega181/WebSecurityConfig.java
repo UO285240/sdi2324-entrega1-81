2dc1ad3a99c6fa705df6701e3734764043d9691c
Javier Monteserín Rodríguez <uo288524@uniovi.es>
2024-03-01 23:30:14 +0100

    Arreglado bug signup

--- src/main/java/com/uniovi/sdi2324entrega181/WebSecurityConfig.java
+++ src/main/java/com/uniovi/sdi2324entrega181/WebSecurityConfig.java
@@ -24,5 +24,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().
+                authorizeRequests().
+                antMatchers("/css/**", "/images/**", "/script/**", "/", "/signup", "/login/**").permitAll()
+                .anyRequest().authenticated().
+                and().
+                formLogin().
+                loginPage("/login").
+                permitAll().
+                defaultSuccessUrl("/home").
+                and().
+                logout().
+                permitAll();
+    }
+
+
+
 
 }
